40c573d87f20356eee49244f5f49a585602eee35
livialestingi <livia.lestingi@polimi.it>
2022-06-22 14:46:37 +0200

    setting up structure

--- it/polimi/lsha/energy/main.py
+++ it/polimi/lsha/energy/main.py
@@ -1,1 +1,15 @@
-print('test')
+from utils.logger import Logger
+
+LOGGER = Logger('main')
+
+LOGGER.info("Starting...")
+
+# Parse .dot file
+
+# Convert to Uppaal model
+
+# Run Verification
+
+# Analyze Results
+
+LOGGER.info("Done.")
---
+++ resources/upp_resources/verify.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+$1/verifyta $2 $3 | tee $4
