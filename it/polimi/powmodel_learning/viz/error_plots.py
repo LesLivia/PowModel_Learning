5a58771e95209973b39fe216c18e5006c50eb0c5
livialestingi <livia.lestingi@polimi.it>
2022-07-07 16:11:29 +0200

    bug fixing

--- it/polimi/powmodel_learning/viz/error_plots.py
+++ it/polimi/powmodel_learning/viz/error_plots.py
@@ -9,7 +9,7 @@ plt.figure(figsize=(30, 5))
 with open(log_name) as log:
     lines = log.readlines()
     errors = [float(line.split('\t')[1].replace('%\n', '')) for i, line in enumerate(lines)
-              if lines[i - 1].__contains__("ERROR")]
+              if lines[i - 1].__contains__("ESTIMATION ERROR")]
     plt.hist(errors, bins=100)
-    plt.xticks(np.arange(0, 2600, 50))
+    plt.xticks(np.arange(0, max(errors), 50))
     plt.show()
