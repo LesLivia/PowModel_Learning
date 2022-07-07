14356ce34ad78fda25e2819456545d0d449bee91
livialestingi <livia.lestingi@polimi.it>
2022-07-07 17:30:36 +0200

    bug fixing

--- it/polimi/powmodel_learning/viz/error_plots.py
+++ it/polimi/powmodel_learning/viz/error_plots.py
@@ -1,4 +1,5 @@
 import sys
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,6 +11,11 @@ with open(log_name) as log:
     lines = log.readlines()
     errors = [float(line.split('\t')[1].replace('%\n', '')) for i, line in enumerate(lines)
               if lines[i - 1].__contains__("ESTIMATION ERROR")]
+    avg_error = sum(errors) / len(errors)
+    print("Average error: {:.3f}% on {} eligible traces.".format(avg_error, len(errors)))
     plt.hist(errors, bins=100)
+    plt.axvline(avg_error, color='k', linestyle='dashed')
+    min_ylim, max_ylim = plt.ylim()
+    plt.text(avg_error+1.0, max_ylim * 0.9, "Mean {:.1f}".format(avg_error))
     plt.xticks(np.arange(0, max(errors), 50))
     plt.show()
