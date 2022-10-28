dd33b6e23807577d8d57d8ca9ba82679282ec6c5
livialestingi <livia.lestingi@polimi.it>
2022-10-28 10:07:39 +0200

    bug fixing

--- it/polimi/powmodel_learning/mgrs/SHA2Upp.py
+++ it/polimi/powmodel_learning/mgrs/SHA2Upp.py
@@ -196,7 +196,7 @@ def generate_upp_model(learned_sha: SHA, trace_day: str, validation=False, tt=No
     else:
         time_bound = TAU
 
-    nta_tplt = nta_tplt.replace('**TIME_BOUND**', str(10) + ';\n')
+    nta_tplt = nta_tplt.replace('**TIME_BOUND**', str(time_bound) + ';\n')
 
     nta_tplt = nta_tplt.replace('**EPS**', EPS)
 
